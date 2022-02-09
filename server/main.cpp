6aaf0542e3d31874ca513ba53f9d640a298935d3
YisakH <drm05003@naver.com>
2022-02-09 11:31:33 +0900

    메시지 지속 전송 수정

--- server/main.cpp
+++ server/main.cpp
@@ -6,7 +6,8 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc <= 1){
+    if (argc <= 1)
+    {
         printf("argument must be required\n");
         exit(1);
     }
@@ -24,67 +25,81 @@ int main(int argc, char *argv[])
 
     //-------------------------------------------------------------------------
 
-
     myRDMA myrdma[NUM_DEST];
     vector<int> sockList = myrdmaTcp.getValidSock();
-    vector<map<string,string>> rdmaInfo;
+    vector<map<string, string>> rdmaInfo;
     char send_buffer[4][1024];
-    char recv_buffer[4][1024];
+    // char recv_buffer[4][1024];
 
-    for(int i=0; i<sockList.size(); i++){
-        //char send_buffer[1024];
-        myrdma[i].makeRDMAqp(recv_buffer[i], sizeof(recv_buffer));
+    for (int i = 0; i < sockList.size(); i++)
+    {
+        // char send_buffer[1024];
+        myrdma[i].makeRDMAqp(send_buffer[i], sizeof(send_buffer));
 
         std::ostringstream oss;
-        oss << &recv_buffer[i];
-        myrdmaTcp.send_msg(i, oss.str()+"\n");
-        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.mr->length)+"\n");
-        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.mr->lkey)+"\n");
-        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.mr->rkey)+"\n");
-        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.lid)+"\n");
-        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.qp_num)+"\n");
+        oss << &send_buffer[i];
+        myrdmaTcp.send_msg(i, oss.str() + "\n");
+        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.mr->length) + "\n");
+        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.mr->lkey) + "\n");
+        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.mr->rkey) + "\n");
+        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.lid) + "\n");
+        myrdmaTcp.send_msg(i, to_string(myrdma[i].rdmaBaseData.qp_num) + "\n");
     }
-    
+
     sleep(2);
 
-    //Send RDMA info
+    // Send RDMA info
 
-    //Read RDMA info
+    // Read RDMA info
 
-    for(int i=0; i<sockList.size(); i++){
-        map<string,string> returnVal = myrdmaTcp.readRDMAInfo(sockList[i]);
+    for (int i = 0; i < sockList.size(); i++)
+    {
+        map<string, string> returnVal = myrdmaTcp.readRDMAInfo(sockList[i]);
         rdmaInfo.push_back(returnVal);
     }
-    //map<string, string> rdmaInfo = myrdmaTcp.readRDMAInfo();
+    // map<string, string> rdmaInfo = myrdmaTcp.readRDMAInfo();
 
-    //Exchange queue pair state
-    for(int i=0; i<sockList.size(); i++){
+    // Exchange queue pair state
+    for (int i = 0; i < sockList.size(); i++)
+    {
         myrdma[i].changeQueuePairStateToInit(myrdma[i].rdmaBaseData.qp);
         myrdma[i].changeQueuePairStateToRTR(myrdma[i].rdmaBaseData.qp, PORT, stoi(rdmaInfo[i].find("qp_num")->second), stoi(rdmaInfo[i].find("lid")->second));
         myrdma[i].changeQueuePairStateToRTS(myrdma[i].rdmaBaseData.qp);
     }
 
+    if (serverMode)
+    {
+        while (true)
+        {
+            sleep(5);
 
-    if(serverMode){
-        sleep(10);
-        
-        for(int i=0; i<sockList.size(); i++){
-            cout << recv_buffer[i] << endl;
+            for (int i = 0; i < sockList.size(); i++)
+            {
+                cout << send_buffer[i] << endl;
+            }
         }
     }
-    else{ // client mode
-        string sss = string(argv[1]) + " : Hi";
-        for(int i=0; i<sockList.size(); i++){
-            strcpy(send_buffer[i], sss.c_str());
-            myrdma[i].post_rdma_write(myrdma[i].rdmaBaseData.qp, myrdma[i].rdmaBaseData.mr,
-                send_buffer[i], sizeof(char)*1024, rdmaInfo[i].find("addr")->second, rdmaInfo[i].find("rkey")->second);
-            myrdma[i].pollCompletion(myrdma[i].rdmaBaseData.completion_queue);
+    else
+    { // client mode
+        while (true)
+        {
+            string input;
+            cin >> input;
+            string sss = string(argv[1]) + " : " + input;
+            for (int i = 0; i < sockList.size(); i++)
+            {
+                strcpy(send_buffer[i], sss.c_str());
+                myrdma[i].post_rdma_write(myrdma[i].rdmaBaseData.qp, myrdma[i].rdmaBaseData.mr,
+                                          send_buffer[i], sizeof(char) * 1024, rdmaInfo[i].find("addr")->second, rdmaInfo[i].find("rkey")->second);
+                myrdma[i].pollCompletion(myrdma[i].rdmaBaseData.completion_queue);
+            }
         }
     }
 
     sleep(3);
-    
-    for(int i=0; i<sockList.size(); i++){
+
+    for (int i = 0; i < sockList.size(); i++)
+    {
         ibv_destroy_qp(myrdma[i].rdmaBaseData.qp);
         ibv_destroy_cq(myrdma[i].rdmaBaseData.completion_queue);
         ibv_dereg_mr(myrdma[i].rdmaBaseData.mr);
