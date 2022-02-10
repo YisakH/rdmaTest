2fe3d1e7569d3c36f5a25a1e8e67c3fe5872efe4
YisakH <drm05003@naver.com>
2022-02-10 13:54:49 +0900

    3번 수신되는 에러 수정

--- server/main.cpp
+++ server/main.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
             cin.getline(input, sizeof(input));
 
             string sss = string(argv[1]) + " : " + input;
-            for (int i = 0; i < sockList.size(); i++)           // 수정 필요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            for (int i = 0; i < sockList.size(); i++)          
             {
                 strcpy(send_buffer[i], input);
                 myrdma[i].post_rdma_write(myrdma[i].rdmaBaseData.qp,
--- server/src/my/myrdma.cpp
+++ server/src/my/myrdma.cpp
@@ -1,9 +1,7 @@
 #include "myrdma.hpp"
 #include "thread"
 
-
-
-void myRDMA::makeRDMAqp(char * send_buffer, int buffer_size)
+void myRDMA::makeRDMAqp(char *send_buffer, int buffer_size)
 {
     rdma = new RDMA;
     this->send_buffer = send_buffer;
@@ -11,9 +9,8 @@ void myRDMA::makeRDMAqp(char * send_buffer, int buffer_size)
 
     rdmaBaseData.context = rdma->createContext();
     rdmaBaseData.protection_domain = ibv_alloc_pd(rdmaBaseData.context);
-    rdmaBaseData.cq_size = 0x10;    
+    rdmaBaseData.cq_size = 0x10;
     rdmaBaseData.comp_channel = ibv_create_comp_channel(rdmaBaseData.context);
-
 
     rdmaBaseData.completion_queue = ibv_create_cq(rdmaBaseData.context, rdmaBaseData.cq_size, cq_context, rdmaBaseData.comp_channel, 1);
     rdmaBaseData.qp = rdma->createQueuePair(rdmaBaseData.protection_domain, rdmaBaseData.completion_queue);
@@ -21,19 +18,18 @@ void myRDMA::makeRDMAqp(char * send_buffer, int buffer_size)
     rdmaBaseData.mr = rdma->registerMemoryRegion(rdmaBaseData.protection_domain, send_buffer, buffer_size);
     rdmaBaseData.lid = rdma->getLocalId(rdmaBaseData.context, PORT);
     rdmaBaseData.qp_num = rdma->getQueuePairNumber(rdmaBaseData.qp);
-
 }
 
 void myRDMA::readRDMAMsg(int sizeofNode)
 {
-    thread t(&myRDMA::readRDMAMsg_t, this,   sizeofNode);
+    thread t(&myRDMA::readRDMAMsg_t, this, sizeofNode);
 
     t.detach();
 }
 
 void myRDMA::readRDMAMsg_t(int sizeofNode)
 {
-    //ibv_req_notify_cq(rdmaBaseData.completion_queue, 0);
+    // ibv_req_notify_cq(rdmaBaseData.completion_queue, 0);
 
     while (true)
     {
@@ -42,14 +38,12 @@ void myRDMA::readRDMAMsg_t(int sizeofNode)
         printf("수신 실행됨\n");
         tempRecv();
 
-        //ibv_get_cq_event(rdmaBaseData.comp_channel, &rdmaBaseData.completion_queue, &cq_context);
-        //ibv_req_notify_cq(rdmaBaseData.completion_queue, 0);
+        // ibv_get_cq_event(rdmaBaseData.comp_channel, &rdmaBaseData.completion_queue, &cq_context);
+        // ibv_req_notify_cq(rdmaBaseData.completion_queue, 0);
 
         pollCompletion(rdmaBaseData.completion_queue);
-        
-        for(int i=0; i<sizeofNode; i++){
-            printf("%s\n", send_buffer);
-        }
+
+        printf("%s\n", send_buffer);
     }
     ibv_ack_cq_events(rdmaBaseData.completion_queue, 1);
 }
